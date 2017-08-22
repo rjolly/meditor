2de5db4caf06939ba5c0101247861c3d49e67e20
Raphael Jolly <rjolly@users.sourceforge.net>
2017-08-22 13:47:59 +0200

    empty

--- src/jscl/editor/Editor.java
+++ src/jscl/editor/Editor.java
@@ -683,7 +683,7 @@ public class Editor extends ScriptSupport {
 	public Editor() {
 		initComponents();
 		setIcon(new ImageIcon(getClass().getResource("meditor24.png")));
-		setMimeType("text/plain");
+		setMimeType("text/plain:text/*");
 		chooser.setFileFilter(new FileNameExtensionFilter("Text", "txt"));
 		cutAction.putValue(Action.NAME, "Cut");
 		cutAction.putValue(Action.SMALL_ICON, cutIcon16);
