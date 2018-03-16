6d7d3b8ff8848d74bff9c686f703310c1c5789ff
Raphael Jolly <rjolly@users.sourceforge.net>
2018-03-16 14:22:01 +0100

    empty

--- src/jscl/editor/TeX.java
+++ src/jscl/editor/TeX.java
@@ -1,18 +1,36 @@
 package jscl.editor;
 
 import java.awt.Image;
+import java.io.IOException;
+import uk.ac.ed.ph.snuggletex.SnuggleInput;
+import uk.ac.ed.ph.snuggletex.SnuggleEngine;
+import uk.ac.ed.ph.snuggletex.SnuggleSession;
+import org.w3c.dom.Element;
 
 public class TeX {
 	public static final TeX instance = new TeX();
+	private SnuggleEngine engine = new SnuggleEngine();
 
 	private TeX() {
 	}
 
-	String mml(final String str) {
-		return null;
+	private SnuggleSession session(final String str) throws IOException {
+		final SnuggleSession session = engine.createSession();
+		session.parseInput(new SnuggleInput(stripMath(str)));
+		return session;
 	}
 
-	Image createImage(final String str) {
-		return null;
+	String mml(final String str) throws IOException {
+		return session(str).buildXMLString();
+	}
+
+	Image createImage(final String str) throws Exception {
+		final Element e = (Element) session(str).buildDOMSubtree().item(0);
+		e.setAttribute("color", "red");
+		return MathML.instance.createImage(e);
+	}
+
+	private String stripMath(final String str) {
+		return "$" + str.substring(0, str.length() - 7).substring(6) + "$";
 	}
 }
