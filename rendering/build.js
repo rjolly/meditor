4e90b17650163e57dfd5a4467c6b409e5bbad49f
Raphael Jolly <rjolly@users.sourceforge.net>
2020-05-23 17:51:24 +0200

    empty

---
+++ rendering/build.js
@@ -0,0 +1,17 @@
+mkdir("build");
+mkdir("build/classes");
+mkdir("build/sources");
+mkdir("build/javadoc");
+
+javac("src", "build/classes");
+copy("src", "build/sources");
+javadoc("src", "build/javadoc");
+
+mkdir("dist");
+var name = "rendering";
+jar("dist/" + name + ".jar", "build/classes");
+jar("dist/" + name + "-source.jar", "build/sources");
+jar("dist/" + name + "-javadoc.jar", "build/javadoc");
+cp("pom.xml", "dist/" + name + ".pom")
+
+publish("dist")
