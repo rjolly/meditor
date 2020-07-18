cbe94d48aefa1c4d8718ea29fb8345ef5d7fba91
Raphael Jolly <rjolly@users.sourceforge.net>
2020-07-18 09:12:37 +0200

    empty

--- src/jscl/editor/SVG.java
+++ src/jscl/editor/SVG.java
@@ -2,9 +2,11 @@ package jscl.editor;
 
 import java.awt.Image;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.io.Writer;
 import java.io.StringReader;
 import java.io.StringWriter;
+import javax.swing.ImageIcon;
 import org.apache.batik.svggen.SVGGraphics2D;
 import org.apache.batik.svggen.SVGGraphics2DIOException;
 import org.apache.batik.anim.dom.SVGDOMImplementation;
@@ -14,13 +16,15 @@ import org.apache.batik.transcoder.TranscoderException;
 
 public class SVG {
 	public static final SVG instance = new SVG();
+	private final Image bookmark = new ImageIcon(getClass().getResource("/toolbarButtonGraphics/general/Bookmarks16.gif")).getImage();
 
 	private SVG() {
 	}
 
 	public String print(final Component comp) {
 		final SVGGraphics2D g = new SVGGraphics2D(SVGDOMImplementation.getDOMImplementation().createDocument(SVGDOMImplementation.SVG_NAMESPACE_URI, "svg", null));
-		g.setSVGCanvasSize(comp.getSize());
+		final Dimension size = comp.getSize();
+		g.setSVGCanvasSize(size);
 		comp.print(g);
 		final Writer writer = new StringWriter();
 		try {
@@ -29,7 +33,7 @@ public class SVG {
 			e.printStackTrace();
 		}
 		String s = writer.toString();
-		s = s.replaceAll("\r", "");
+		s = s.replaceAll(" NaN", " " + size.getHeight()).replaceAll("\r", "");
 		for (int i = 0 ; i < 3 ; i++) s = s.substring(s.indexOf('\n') + 1);
 		return s.substring(0, s.lastIndexOf('\n'));
 	}
@@ -38,9 +42,10 @@ public class SVG {
 		final MemoryTranscoder transcoder = new MemoryTranscoder();
 		try {
 			transcoder.transcode(new TranscoderInput(new StringReader(document)), new TranscoderOutput());
+			return transcoder.getImage();
 		} catch (final TranscoderException e) {
 			e.printStackTrace();
 		}
-		return transcoder.getImage();
+		return bookmark;
 	}
 }
